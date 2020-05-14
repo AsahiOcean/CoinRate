955bdc1d13a60aabea47ed40c915ace18ea8fcdb
AsahiOcean <sergfedotov@icloud.com>
2020-05-14 09:10:09 +0300

    Project сreated

---
+++ CoinRate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinRate
+//
+//  Created by Serg Fedotov on 14.05.2020.
+//  Copyright © 2020 Sergey Fedotov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
